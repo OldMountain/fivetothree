c1aa5f9ee670a8f6f1c9a3bf922318adc7df9036
卢杭启 <itoutsource.cz45@trinasolar.com>
2018-10-31 13:50:55 +0800

    日志、权限 自定义注解

---
+++ src/main/java/com/nxd/ftt/annotation/LogAndPermission.java
@@ -0,0 +1,64 @@
+package com.nxd.ftt.annotation;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.lang.annotation.*;
+
+/**
+ * 日志、权限
+ *
+ * @author OldMountain
+ * @date 2018/10/31
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@RequestMapping
+public @interface LogAndPermission {
+
+    String[] permissions() default {};
+
+    @AliasFor(
+            annotation = RequestMapping.class,
+            attribute = "value"
+    )
+    String[] value() default {};
+
+    @AliasFor(
+            annotation = RequestMapping.class,
+            attribute = "path"
+    )
+    String[] path() default {};
+
+    @AliasFor(
+            annotation = RequestMapping.class,
+            attribute = "method"
+    )
+    RequestMethod[] method() default {};
+
+    @AliasFor(
+            annotation = RequestMapping.class,
+            attribute = "params"
+    )
+    String[] params() default {};
+
+    @AliasFor(
+            annotation = RequestMapping.class,
+            attribute = "headers"
+    )
+    String[] headers() default {};
+
+    @AliasFor(
+            annotation = RequestMapping.class,
+            attribute = "consumes"
+    )
+    String[] consumes() default {};
+
+    @AliasFor(
+            annotation = RequestMapping.class,
+            attribute = "produces"
+    )
+    String[] produces() default {};
+}
